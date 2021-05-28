a9d45c76f13036ddbee98719d78ac8ad7574ee1c
NatalieGFonseca <69815597+NatalieGFonseca@users.noreply.github.com>
2021-05-28 16:12:20 -0300

    Criação da classe contaVip

--- Prototype/src/entidades/contaVip.java
+++ Prototype/src/entidades/contaVip.java
@@ -1,23 +1,89 @@
 package entidades;
 
-public class contaVip implements Conta{
+import java.util.Scanner;
 
+public class contaVip implements Conta{
+	double limite = 5000;
+	double saque, deposito, emprestimo, saldo;
+	
+	public contaVip() {
+		
+	}
+	
+	public contaVip(contaVip contaVip) {
+		int op= 0;
+		Scanner entrada = new Scanner(System.in);
+		
+		do {
+			System.out.println("Bem-vindo, Cliente VIP");
+			System.out.println("\n1 - Saque");
+			System.out.println("\n2 - Depósito");
+			System.out.println("\n3 - Empréstimo");
+			System.out.println("\n4- Sair");
+			System.out.println("Opção desejada: ");
+			op = entrada.nextInt();
+			
+			switch(op) {
+			case 1:
+				System.out.println("Valor de saque: ");
+				saque = entrada.nextDouble();
+				contaVip.sacar(saque);
+				break;
+			case 2:
+				System.out.println("Valor do depósito: ");
+				deposito = entrada.nextDouble();
+				contaVip.depositar(deposito);
+				break;
+			case 3:
+				System.out.println("Valor do empréstimo: ");
+				emprestimo = entrada.nextDouble();
+				contaVip.fazerEmprestimo(emprestimo);
+				break;
+			case 4:
+				System.out.printf("\nFechando sistema");
+                System.exit(1);
+                break;
+                
+                default:
+                	break;
+                
+			}
+		}while(op>0 && op<4);
+	}
+	
 	@Override
 	public void sacar(double valorAsacar) {
-		// TODO Auto-generated method stub
-		
+		if(valorAsacar > (saldo+limite)) {
+			System.out.println("Saldo insuficiente");
+		}else {
+			saldo -= valorAsacar;
+			System.out.println("Saldo após a operação: "+saldo);
+		}
 	}
 
 	@Override
 	public void depositar(double valorAdepositar) {
-		// TODO Auto-generated method stub
-		
+		saldo += valorAdepositar;
+	}
+	
+	public void fazerEmprestimo(double valorAemprestar) {
+		if(valorAemprestar <= limite) {
+			emprestimo = valorAemprestar;
+			limite -= valorAemprestar;
+			System.out.println("Limite de empréstimo disponível após operação: "+limite);
+		}else {
+			if(valorAemprestar > limite) {
+				System.out.println("Limite excedido");
+			}else {
+				System.out.println("O limite já foi utilizado");
+			}
+		}
 	}
 
 	@Override
 	public Conta clone() {
-		// TODO Auto-generated method stub
-		return null;
+		return new contaVip(this);
 	}
 
 }
+
