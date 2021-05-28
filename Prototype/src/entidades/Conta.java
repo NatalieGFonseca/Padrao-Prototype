f7b4869bfcee23917114e68cc5ec7d6de877bea4
natyesilva <78439511+natyesilva@users.noreply.github.com>
2021-05-28 18:57:19 -0300

    Update Conta.java

--- Prototype/src/entidades/Conta.java
+++ Prototype/src/entidades/Conta.java
@@ -1,7 +1,10 @@
 package entidades;
 
 public interface Conta {
+	
+  
 	public void sacar(double valorAsacar);
 	public void depositar(double valorAdepositar);
 	public abstract Conta clone();
+	
 }
