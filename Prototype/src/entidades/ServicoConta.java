1a9bc5ad945c5ed5262f61a223058a7d9050d321
NatalieGFonseca <69815597+NatalieGFonseca@users.noreply.github.com>
2021-06-06 13:48:38 -0300

    Delete Prototype/src/entidades directory

--- Prototype/src/entidades/ServicoConta.java
+++
@@ -1,8 +0,0 @@
-package entidades;
-
-public interface ServicoConta {
-	
-	public abstract void sacar(Double valorASacar);
-	public abstract void depositar(Double valorADepositar);
-	public abstract ServicoConta clonar();
-}
